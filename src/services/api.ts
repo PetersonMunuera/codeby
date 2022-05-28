2a11440c91f0bd7ef5630987bd58bb65289716e5
Peterson Munuera <peterson.munuera@somosdx.co>
2022-05-28 03:33:56 -0300

    baseURL changed

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://6291c19cb6692246ddc922f1--frabjous-belekoy-15acef.netlify.app:3000'
 });
